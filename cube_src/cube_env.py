1d0e7f76bf3890162aa7bd75dd619a085efe07c9
Rive Sunder <rivesunder@protonmail.com>
2019-10-22 13:46:10 +0100

    add basic RL functionality to cube environment

--- cube_src/cube_env.py
+++ cube_src/cube_env.py
@@ -4,9 +4,9 @@ class Cube():
     def __init__(self, difficulty=10):
 
         self.cube = np.zeros((3,3,6))
-        self.action_dim = 6
+        self.action_dim = 12
         self.difficulty = difficulty
-        self.reset()
+        _ = self.reset()
 
 
     def reset(self):
@@ -18,27 +18,74 @@ class Cube():
         for cc in range(self.difficulty):
             self.step(self.get_random_action())
 
+        return self.categorical_cube()
+
     def step(self, action):
         """
         available actions are F, R, L, U, D, B 
         and their reverses
         """
         if action == 0:
-            self.F()
-        elif action == 1:
-            self.R()
-        elif action == 2:
-            self.D()
-        elif action == 3:
-            self.L()
-        elif action == 4:
             self.U()
-        elif action == 5:
+        elif action == 1:
+            self.L()
+        elif action == 2:
+            self.F()
+        elif action == 3:
+            self.R()
+        elif action == 4:
             self.B()
+        elif action == 5:
+            self.D()
+        elif action == 6:
+            self.u()
+        elif action == 7:
+            self.l()
+        elif action == 8:
+            self.f()
+        elif action == 9:
+            self.r()
+        elif action == 10:
+            self.b()
+        elif action == 11:
+            self.d()
+        
+        info = {}
+        done = self.is_solved()
+
+        if done:
+            reward = 32
+        else:
+            reward = -1
+
+        observation = self.categorical_cube()
+
+        return observation, reward, done, info
+
+    def categorical_cube(self):
+
+        categorical_cube = np.zeros((54,6))
+
+        flat_cube = np.copy(self.cube.ravel())
+        for idx in range(len(flat_cube)):
+            categorical_cube[:,int(flat_cube[idx])] = 1.
+
+        return categorical_cube
+
+    def is_solved(self):
+
+        for face in range(6):
+            solve = self.cube[1,1,face] * np.ones_like(self.cube[:,:,face])
+            if np.max(np.abs(solve - self.cube[:,:,face])) > 0:
+                return False
+            
+
+        return True
 
     def get_random_action(self):
         return np.random.randint(self.action_dim)
         
+
     def display_cube(self):
     
         scoot = 10
@@ -79,6 +126,29 @@ class Cube():
     
         self.cube = new_cube
 
+    def f(self):
+        new_cube = np.copy(self.cube)
+
+        new_cube[:,:,2] = self.cube[:,:,2].T
+        for row in [0,2]:
+            new_cube[row,:,2] = self.cube[:,:,2].T[2-row,:]
+
+        temp = self.cube[2,:,0].tolist()
+        temp.reverse()
+        new_cube[:,2,1] = np.array(temp)
+
+        temp = self.cube[:,0,3].tolist()
+        new_cube[2,:,0] = np.array(temp)
+
+        temp = self.cube[0,:,5].tolist()
+        temp.reverse()
+        new_cube[:,0,3] = np.array(temp)
+        
+        temp = self.cube[:,2,1].tolist()
+        new_cube[0,:,5] = np.array(temp)
+    
+        self.cube = new_cube
+
     def B(self):
 
         new_cube = np.copy(self.cube)
@@ -100,6 +170,30 @@ class Cube():
     
         temp = self.cube[:,0,1].tolist()
         new_cube[2,:,5] = np.array(temp)
+
+        self.cube = new_cube
+
+    def b(self):
+
+        new_cube = np.copy(self.cube)
+
+        new_cube[:,:,4] = self.cube[:,:,4].T
+        for row in [0,2]:
+            new_cube[row,:,4] = self.cube[:,:,4].T[2-row,:]
+
+        temp = self.cube[:,2,3].tolist()
+        temp.reverse()
+        new_cube[2,:,5] = np.array(temp)
+
+        temp = self.cube[0,:,0].tolist()
+        new_cube[:,2,3] = np.array(temp)
+         
+        temp = self.cube[:,0,1].tolist()
+        temp.reverse()
+        new_cube[0,:,0] = np.array(temp)
+    
+        temp = self.cube[2,:,5].tolist()
+        new_cube[:,0,1] = np.array(temp)
 
         self.cube = new_cube
 
@@ -126,6 +220,28 @@ class Cube():
 
         self.cube = new_cube
 
+    def d(self):
+
+        new_cube = np.copy(self.cube)
+
+        new_cube[:,:,5] = self.cube[:,:,5].T
+        for row in [0,2]:
+            new_cube[row,:,5] = self.cube[:,:,5].T[2-row,:]
+
+        temp = self.cube[2,:,3].tolist()
+        new_cube[2,:,2] = np.array(temp)
+
+        temp = self.cube[2,:,4].tolist()
+        new_cube[2,:,3] = np.array(temp)
+         
+        temp = self.cube[2,:,1].tolist()
+        temp.reverse()
+        new_cube[2,:,4] = np.array(temp)
+    
+        temp = self.cube[2,:,2].tolist()
+        new_cube[2,:,1] = np.array(temp)
+
+        self.cube = new_cube
     def U(self):
         new_cube = np.copy(self.cube)
 
@@ -146,6 +262,29 @@ class Cube():
         temp = self.cube[0,:,1].tolist()
         temp.reverse()
         new_cube[0,:,4] = np.array(temp)
+
+        self.cube = new_cube
+
+    def u(self):
+        new_cube = np.copy(self.cube)
+
+        my_face = 0
+        new_cube[:,:,my_face] = self.cube[:,:,my_face].T
+        for row in [0,2]:
+            new_cube[row,:,my_face] = self.cube[:,:,my_face].T[2-row,:]
+
+        temp = self.cube[0,:,1].tolist()
+        new_cube[0,:,2] = np.array(temp)
+
+        temp = self.cube[0,:,2].tolist()
+        new_cube[0,:,3] = np.array(temp)
+         
+        temp = self.cube[0,:,3].tolist()
+        new_cube[0,:,4] = np.array(temp)
+    
+        temp = self.cube[0,:,4].tolist()
+        temp.reverse()
+        new_cube[0,:,1] = np.array(temp)
 
         self.cube = new_cube
 
@@ -173,6 +312,30 @@ class Cube():
 
         self.cube = new_cube
 
+    def r(self):
+        new_cube = np.copy(self.cube)
+
+        my_face = 3
+        new_cube[:,:,my_face] = self.cube[:,:,my_face].T
+        for row in [0,2]:
+            new_cube[row,:,my_face] = self.cube[:,:,my_face].T[2-row,:]
+
+        temp = self.cube[:,2,0].tolist()
+        new_cube[:,2,2] = np.array(temp)
+
+        temp = self.cube[:,2,2].tolist()
+        new_cube[:,2,5] = np.array(temp)
+         
+        temp = self.cube[:,0,4].tolist()
+        temp.reverse()
+        new_cube[:,2,0] = np.array(temp)
+    
+        temp = self.cube[:,2,5].tolist()
+        temp.reverse()
+        new_cube[:,0,4] = np.array(temp)
+
+        self.cube = new_cube
+
     def L(self):
         new_cube = np.copy(self.cube)
 
@@ -197,24 +360,49 @@ class Cube():
 
         self.cube = new_cube
 
+    def l(self):
+        new_cube = np.copy(self.cube)
+
+        my_face = 1
+        new_cube[:,:,my_face] = self.cube[:,:,my_face].T
+        for row in [0,2]:
+            new_cube[row,:,my_face] = self.cube[:,:,my_face].T[2-row,:]
+
+        temp = self.cube[:,0,5].tolist()
+        new_cube[:,0,2] = np.array(temp)
+
+        temp = self.cube[:,0,0].tolist()
+        temp.reverse()
+        new_cube[:,2,4] = np.array(temp)
+         
+        temp = self.cube[:,0,2].tolist()
+        new_cube[:,0,0] = np.array(temp)
+    
+        temp = self.cube[:,2,4].tolist()
+        temp.reverse()
+        new_cube[:,0,5] = np.array(temp)
+
+        self.cube = new_cube
+
 if __name__ == "__main__":
-    cube = Cube()
 
-    cube.reset()
-    cube.display_cube()
-    cube.F()
-    print("F: ") 
-    cube.display_cube()
-    cube.B()
-    print("B: ") 
-    cube.display_cube()
-    cube.U()
-    print("U: ") 
-    cube.display_cube()
-    cube.R()
-    print("R: ") 
-    cube.display_cube()
-    cube.L()
-    print("L: ") 
-    cube.display_cube()
+    env = Cube(difficulty = 1)
+    obs = env.reset()
 
+
+    done = False
+    steps = []
+    for cc in range(1000):
+        step = 0
+        done = False
+        while not done:
+            _ = env.reset()
+            obs, reward, done, info = env.step(env.get_random_action()) 
+            step += 1
+        print("guessed solving move once in {}".format(step))
+        steps.append(step)
+
+    avg_step = np.mean(steps)
+    std_step = np.std(steps)
+    print("done: {}, reward: {}, avg trials before solve {}+/-{} std dev. ".format(done,reward, avg_step, std_step))
+    env.display_cube()
